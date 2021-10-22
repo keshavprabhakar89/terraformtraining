2eb3e0dbab3fb1c6af141eb17bff2c66e6c37091
keshavprabhakar89 <84273351+keshavprabhakar89@users.noreply.github.com>
2021-10-22 16:56:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.47.0"
+    }
+  }
+ # backend "s3" {
+ #   bucket = "ibm-zeke-training"
+ #   key    = "terraform-backendfile.tfstate"
+ #   region = "ap-south-1"
+ # }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+# Create the bucket
+
+resource "aws_s3_bucket" "tf_code" {
+  bucket        = var.project_name
+  acl           = "private"
+  force_destroy = true
+
+  tags = {
+    Name = "tf_bucket"
+  }
+}
